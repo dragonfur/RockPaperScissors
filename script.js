a9a4fc439acf8325c927d97f8d25b0320fe4ccc4
Jemuel <f2pskilleruel@gmail.com>
2022-03-11 16:58:14 +0300

    Make user input section
    Created a function that will ask for the users input, which will loop
    until user writes either rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //script.js
 
-//function called computerPlay will select random from 3 options
+//function called computerPlay will select random from 1-3 options
 function computerPlay() {
     var decisions = ['Rock','Paper','Scissors'] 
-    var decision = decisions[Math.floor(Math.random()*decisions.length)] //function will generate random number from 0-1, multiply it by 3 and round it down to pick which one it will throw out
-    return decision
+    var computerDecision = decisions[Math.floor(Math.random()*decisions.length)]
+    computerSelection = computerDecision
+    return computerDecision
+}
+
+//function called userInput that will record the user's pick
+function userInput() {
+    do {
+        var playerDecision = prompt("What is your decision?")
+        var playerCheck = playerDecision.toLowerCase()
+        playerDecision = playerCheck.charAt(0).toUpperCase() + playerCheck.slice(1)
+    }
+    while (playerCheck != "rock" && playerCheck != "scissors" && playerCheck != "paper");
+    //playerSelection = playerDecision
+
 }
