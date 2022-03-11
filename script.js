b80fd563f97513d97a5c35e67bc1bd2f7958908c
Jemuel <f2pskilleruel@gmail.com>
2022-03-11 17:37:10 +0300

    Make one playable round
    Set it up so that it will simulate first the computers decision, then
    ask for the players input until it receives the right one (paper,
    scissors, or rock). Then it will check using the script to compare
    the values for each variable and return the right winner.

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 //script.js
 
-//function called computerPlay will select random from 1-3 options
+//function that simulates computer's decision by generating rand num.
 function computerPlay() {
-    var decisions = ['Rock','Paper','Scissors'] 
+    var decisions = ['rock','paper','scissors'] 
     var computerDecision = decisions[Math.floor(Math.random()*decisions.length)]
-    computerSelection = computerDecision
     return computerDecision
-}
+    }
 
-//function called userInput that will record the user's pick
-function userInput() {
+//simulation of one round
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay()
     do {
         var playerDecision = prompt("What is your decision?")
         var playerCheck = playerDecision.toLowerCase()
-        playerDecision = playerCheck.charAt(0).toUpperCase() + playerCheck.slice(1)
+        playerSelection = playerCheck
+        var tiegame = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)
+
+        if (playerSelection == computerSelection)
+            return ("Tie game! You both selected " + tiegame)
+        else if (playerSelection == "paper" && computerSelection == "rock")
+            return ("You win! Paper beats Rock!")
+        else if (playerSelection == "paper" && computerSelection == "scissors")
+            return ("You lose! Scissors beats Paper!")
+        else if (playerSelection == "scissors" && computerSelection == "rock")
+            return ("You lose! Rock beats Scissors!")
+        else if (playerSelection == "scissors" && computerSelection == "paper")
+            return ("You win! Scissors beats Paper!")
+        else if (playerSelection == "rock" && computerSelection == "paper")
+            return ("You lose! Paper beats Rock!")
+        else (playerSelection == "rock" && computerSelection == "scissors")
+            return ("You win! Rock beats Scissors!")
     }
     while (playerCheck != "rock" && playerCheck != "scissors" && playerCheck != "paper");
-    //playerSelection = playerDecision
-
+    
 }
+
+console.log
