d751b9550a5198e549f128da23d24261834f394b
Jemuel <f2pskilleruel@gmail.com>
2022-03-11 17:42:02 +0300

    Clean up code
    Separated the comparisons from the do-while section to avoid further
    errors in the future and also deleted the rogue console.log at the end.

--- script.js
+++ script.js
@@ -16,23 +16,21 @@ function playRound(playerSelection, computerSelection) {
         playerSelection = playerCheck
         var tiegame = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)
 
-        if (playerSelection == computerSelection)
-            return ("Tie game! You both selected " + tiegame)
-        else if (playerSelection == "paper" && computerSelection == "rock")
-            return ("You win! Paper beats Rock!")
-        else if (playerSelection == "paper" && computerSelection == "scissors")
-            return ("You lose! Scissors beats Paper!")
-        else if (playerSelection == "scissors" && computerSelection == "rock")
-            return ("You lose! Rock beats Scissors!")
-        else if (playerSelection == "scissors" && computerSelection == "paper")
-            return ("You win! Scissors beats Paper!")
-        else if (playerSelection == "rock" && computerSelection == "paper")
-            return ("You lose! Paper beats Rock!")
-        else (playerSelection == "rock" && computerSelection == "scissors")
-            return ("You win! Rock beats Scissors!")
     }
     while (playerCheck != "rock" && playerCheck != "scissors" && playerCheck != "paper");
     
+    if (playerSelection == computerSelection)
+        return ("Tie game! You both selected " + tiegame)
+    else if (playerSelection == "paper" && computerSelection == "rock")
+        return ("You win! Paper beats Rock!")
+    else if (playerSelection == "paper" && computerSelection == "scissors")
+        return ("You lose! Scissors beats Paper!")
+    else if (playerSelection == "scissors" && computerSelection == "rock")
+        return ("You lose! Rock beats Scissors!")
+    else if (playerSelection == "scissors" && computerSelection == "paper")
+        return ("You win! Scissors beats Paper!")
+    else if (playerSelection == "rock" && computerSelection == "paper")
+        return ("You lose! Paper beats Rock!")
+    else (playerSelection == "rock" && computerSelection == "scissors")
+        return ("You win! Rock beats Scissors!")
 }
-
-console.log
