2534b00218737f96d54d5998c0eadcdffaa5e57e
Jemuel <f2pskilleruel@gmail.com>
2022-03-13 19:53:10 +0300

    Make a ui
    Changed the script to include a UI function that will let you play
    by clicking the buttons. I made it so that the buttons will disappear
    once someone wins preventing destruction of code.

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 //script.js
 
+var computerScore = 0
+var playerScore = 0
+
+const results = document.querySelector(".results")
+const winner = document.createElement("div")
+const content = document.createElement("div")
+const playerResults = document.createElement("div")
+const computerResults = document.createElement("div")
+
+//keeps track of scores and updates in real time
+function keepingScore(){
+    playerResults.textContent = "Player score: " + playerScore 
+    computerResults.textContent = "Computer score: " + computerScore
+    results.appendChild(playerResults)
+    results.appendChild(computerResults)
+}
+
+keepingScore()
+
+//checks for winner and displays who won
+function theWinner() {
+    if (playerScore == 5) {
+        winner.textContent = "You won with a score of " + playerScore + " to the computer's " + computerScore
+        results.appendChild(winner)
+    
+        //stops play
+        rock.remove("#rock")
+        paper.remove("#paper")
+        scissors.remove("#scissors")
+    }
+    else if (computerScore == 5) {
+        winner.textContent = "You lost with a score of " + playerScore + " to the computer's " + computerScore
+        results.appendChild(winner)
+        
+        //stops play
+        rock.remove("#rock")
+        paper.remove("#paper")
+        scissors.remove("#scissors")
+    }
+}
+
 //function that simulates computer's decision by generating rand num.
 function computerPlay() {
     var decisions = ['rock','paper','scissors'] 
@@ -10,64 +51,75 @@ function computerPlay() {
 //simulation of one round
 function playRound(playerSelection, computerSelection) {
     var playerCheck = playerSelection.toLowerCase()
+    computerSelection = computerPlay()
+    var computerCapital = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
     
     if (playerCheck == computerSelection) {
-        return 0
+        content.textContent = "You tied! You both picked " + computerCapital + "!"
+        results.appendChild(content)
+        
     }
     else if (playerCheck == "paper" && computerSelection == "rock"){
-        return 1
+        playerScore++
+        keepingScore()
+        content.textContent = "You won! Paper beats Rock!"
+        results.appendChild(content)
+        theWinner()
+        return
     }
     else if (playerCheck == "paper" && computerSelection == "scissors"){
-        return 2
-        }
+        computerScore++
+        keepingScore()
+        content.textContent = "You lost! Scissors beats Paper"
+        results.appendChild(content)
+        theWinner()
+        return
+    }
     else if (playerCheck == "scissors" && computerSelection == "rock"){
-        return 2
-        }
+        computerScore++
+        keepingScore()
+        content.textContent = "You lost! Rock beats Scissors"
+        results.appendChild(content)
+        theWinner()
+        return
+    }
     else if (playerCheck== "scissors" && computerSelection == "paper"){
-        return 1
+        playerScore++
+        keepingScore()
+        content.textContent = "You won! Scissors beats Paper!"
+        results.appendChild(content)
+        theWinner()
+        return
     }
     else if (playerCheck == "rock" && computerSelection == "paper"){
-        return 2
-        }
+        computerScore++
+        keepingScore()
+        content.textContent = "You lost! Rock beats Paper"
+        results.appendChild(content)
+        theWinner()
+        return
+    }
     else (playerCheck == "rock" && computerSelection == "scissors")
-        return 1
+        playerScore++
+        keepingScore()
+        content.textContent = "You won! Rock beats Scissors!"
+        results.appendChild(content)
+        theWinner()
+        return
 }
 
-//simulates 5 rounds of a game, best of 5
-function game(){
-    var playerSelection
-    var computerSelection
-    var result 
-    var userScore = 0
-    var computerScore = 0
+//listening for button actions then sending them to playRound()
+const rock = document.querySelector("#rock")
+rock.addEventListener("click", () => {
+    playRound("rock")
+})
 
-    //loop for rounds
-    for (let i = 0; i < 5; i++) {
-        var round = i + 1
-        playerSelection = prompt("What is your decision?")
-        computerSelection = computerPlay()
-        result = (playRound(playerSelection, computerSelection))
-        var compCapitalize = computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)
-        var playerCheck = playerSelection.toLowerCase()
-        var tiegame = playerCheck.charAt(0).toUpperCase()+playerCheck.slice(1)
-        console.log("Round " + round)
-        if (result == 1){
-            userScore++
-            console.log("You win! " + tiegame + " beats " + compCapitalize + "!")
-        }
-        else if (result == 2){
-            computerScore++
-            console.log("You lose! " + compCapitalize + " beats " + tiegame + "!")
-        }
-        else if (result == 0)
-            console.log("Tie game! you both picked " + tiegame + "!")
-     }
-     //displays score after 5 rounds
-     if (userScore > computerScore)
-        console.log("You win after 5 rounds with a score of " + userScore + " to the computer's " + computerScore)
-    else if (userScore < computerScore)
-        console.log("You lost after 5 rounds with a score of " + userScore + " to the computer's " + computerScore)
-    else if (userScore == computerScore)
-        console.log("You drew after 5 rounds with a score of " + userScore + " to the computer's " + computerScore)
+const paper = document.querySelector("#paper")
+paper.addEventListener("click", () => {
+    playRound("paper")
+})
 
-}
+const scissors = document.querySelector("#scissors")
+scissors.addEventListener("click", () => {
+    playRound("scissors")
+})
